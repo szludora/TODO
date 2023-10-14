aac441e0a9b84fbc76e97395264f066a5addab07
TAlexa1997 <alejandra.marianella.8@gmail.com>
2023-10-14 11:11:07 +0200

    for ciklus helyett forEach

--- Megjelenit.js
+++ Megjelenit.js
@@ -1,23 +1,25 @@
 import MegjelenitSor from "./MegjelenitSor.js";
 
 class Megjelenit {
-    #list = [];
-    constructor(list, szuloElem) {
-        this.#list = list;
-        szuloElem.append(
-            '<table class="table table-bordered table-striped  ">'
-        );
-        this.tablaElem = szuloElem.children("table");
+  #list = [];
+  constructor(list, szuloElem) {
+    this.#list = list;
+    szuloElem.append('<table class="table table-bordered table-striped  ">');
+    this.tablaElem = szuloElem.children("table");
 
-        this.tablazatbaIr();
+    this.tablazatbaIr();
+  }
+  tablazatbaIr() {
+    // for (let index = 0; index < this.#list.length; index++) {
+    //     const elem = this.#list[index];
 
-    }
-    tablazatbaIr() {
-        for (let index = 0; index < this.#list.length; index++) {
-            const elem = this.#list[index];
+    //     new MegjelenitSor(elem, this.tablaElem);
+    // }
 
-            new MegjelenitSor(elem, this.tablaElem);
-        }
-    }
+    // helyette:
+    this.#list.forEach((elem) => {
+      new MegjelenitSor(elem, this.tablaElem);
+    });
+  }
 }
 export default Megjelenit;
